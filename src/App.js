257b7d2a2f2b5d9896c2be2f738ec07243f6e4a0
Ignacio_Be <rinaldiignacio@gmail.com>
2022-11-09 01:47:22 -0300

    PreEntrega1 Rinaldi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <ItemListContainer greetings={"Hola Mundo"} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
